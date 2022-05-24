44d1a21b4b0cc22d26251feb5d8ca91c15da79ee
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-05-24 21:11:08 +0200

    cambio a nuevo diseño

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Video.css'
+import Typed from 'react-typed';
 
-import spaceVideo from '../../assets/home_video.mp4'
+// import spaceVideo from '../../assets/fondo_liquid.mp4' 
 
 
 const Video = () => {
   return (
     <div className='hero'>
             <video autoPlay loop muted id='video'>
-                <source src={spaceVideo} type='video/mp4'/>
+                {/* <source src={spaceVideo} type='video/mp4'/> */}
             </video>
 
             <div className='content'>
                 <h1>Diseños Impresora 3D</h1>
-                <p>Mejores Diseños Para Imprimir</p>
+                <p>Mejores Diseños Para  
+                  <Typed 
+                  strings={[' Imprimir', ' Comprar', ' Vender']}
+                  typeSpeed={120}
+                  backSpeed={140}
+                  loop
+                  />
+
+                </p>
             
             <div>
                 <Link to='/training' className='btn'>Comprar</Link>
