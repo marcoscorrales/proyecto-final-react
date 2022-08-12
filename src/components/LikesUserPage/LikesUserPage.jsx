769842abe72b524aa7d3333ed645d8b3d2889389
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-08-12 09:41:15 +0200

    Añado llamada api productsUserPage y likesUserPage

--- src/components/LikesUserPage/LikesUserPage.jsx
+++ src/components/LikesUserPage/LikesUserPage.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './LikesUserPage.css'
 import { useParams } from 'react-router-dom';
 import usuarioimg from '../../assets/images/usuario.png'
 import product from '../../assets/images/sponsor1.jpg'
 import ProductCard from '../ProductCard/ProductCard';
+import axios from 'axios';
+
+const endpoint= 'http://127.0.0.1:8000/api'
 
 const LikesUserPage = () => {
   let parametros = useParams();
   let usuario = (parseInt(parametros.id));
 
-  const elements = ['Marcos Corrales', 'Marcos Corrales', 'Marcos Corrales', 'Marcos Corrales',
-                    'Marcos Corrales', 'Marcos Corrales', 'Marcos Corrales', 'Marcos Corrales',
-                    'Marcos Corrales', 'Marcos Corrales', 'Marcos Corrales', 'Marcos Corrales'];
-  const id = 1;
+  const [modelos, setModelos] = useState([]);
+  const [user, setUser] = useState([]);
+
+    useEffect (()=>{
+  
+      //Devuelve la informacion del modelo
+      const getModelos = async () =>{
+        const response = await axios.get(`${endpoint}/todosultimosmodelos/${usuario}`);
+        setModelos(response.data);
+      }
+
+      const getUser = async () =>{
+        const response = await axios.get(`${endpoint}/usuario/${usuario}`);
+        setUser(response.data);
+      }
+
+      getModelos();
+      getUser();
+    }, [])
 
   return (
     <div className='likesUserPage'>
-    <h1 className='likesUserPage__title'>Todos los likes del Usuario: {usuario}</h1>
+    <h1 className='likesUserPage__title'>Todos los likes del Usuario: {user.nombre}</h1>
     <section className='likesUserPage__cards'>
-            {elements.map((value, index) => {
-                return <ProductCard key={index} id={id} imgProduct={product} title={"Producto"+index} price={"100€"} likes="100" imgAuthor={usuarioimg} author={value}/> 
-            })}
+              {modelos.map((modelo) => {
+                  let precio = 0;
+                  (modelo.precio > 0 ? precio=`${modelo.precio} €` : precio="Gratis");
+
+                  return <ProductCard key={modelo.id} id={modelo.id} imgProduct={product} title={modelo.nombre} price={precio} likes={modelo.likes} imgAuthor={usuarioimg} author={modelo.nombre_user}/> 
+                })}
     </section>
 </div>
 // Falta paginacion
