5e8459e4290c2ba940bcba89c05a7424d7868e52
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-08-23 10:00:07 +0200

    Añado nuevas imagenes sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -6,6 +6,8 @@ import sponsor1 from '../../assets/images/sponsor1.jpg'
 
 import {BsFillMegaphoneFill} from 'react-icons/bs'
 
+const endpoint= 'http://127.0.0.1:8000/uploads/sponsors/'
+
 const Sponsors = () => {
   return (
     <div className='sponsors'>
@@ -15,31 +17,31 @@ const Sponsors = () => {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={imagen__carousel}
+                src={endpoint+"sponsor2.webp"}
                 alt="First slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={imagen__carousel}
+                src={endpoint+"sponsor3.webp"}
                 alt="Second slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={imagen__carousel}
+                src={endpoint+"sponsor4.jpg"}
                 alt="Third slide"
                 />
             </Carousel.Item>
     </Carousel>
 
     <div className='sponsors__imgs'>
-        <div><img src={sponsor1} alt="sponsor" className="sponsor__img img-fluid" /></div>
-        <div><img src={sponsor1} alt="sponsor" className="sponsor__img img-fluid" /></div>
-        <div><img src={sponsor1} alt="sponsor" className="sponsor__img img-fluid" /></div>
-        <div><img src={sponsor1} alt="sponsor" className="sponsor__img img-fluid" /></div>
+        <div><img src={endpoint+"sponsor1.jpg"} alt="sponsor" className="sponsor__img img-fluid" /></div>
+        <div><img src={endpoint+"sponsor2.webp"} alt="sponsor" className="sponsor__img img-fluid" /></div>
+        <div><img src={endpoint+"sponsor3.webp"} alt="sponsor" className="sponsor__img img-fluid" /></div>
+        <div><img src={endpoint+"sponsor4.jpg"} alt="sponsor" className="sponsor__img img-fluid" /></div>
     </div>
 
     </div>
